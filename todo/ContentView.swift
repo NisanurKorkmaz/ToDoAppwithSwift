031947a057b351ec65800ca86e3b47856595edfa
NisanurKorkmaz <nisanur29@live.fr>
2023-03-22 11:31:19 +0300

    update

--- todo/ContentView.swift
+++ todo/ContentView.swift
@@ -7,17 +7,61 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TodoItem: Identifiable {
+    let id = UUID()
+    let task: String
+}
+
+struct TodoList: View {
+    @State var items = [TodoItem]()
+    @State var newTask = ""
+
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("My To-Do List")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.top, 50)
+            
+            HStack {
+                TextField("New task", text: $newTask)
+                Button(action: addTask) {
+                    Text("Add")
+                }
+            }.padding()
+
+            List(items) { item in
+                HStack {
+                    Text(item.task)
+                    Spacer()
+                    Button(action: { removeTask(item) }) {
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+
+    func addTask() {
+        if !newTask.isEmpty {
+            items.append(TodoItem(task: newTask))
+            newTask = ""
+        }
+    }
+
+    func removeTask(_ item: TodoItem) {
+        items.removeAll { $0.id == item.id }
     }
 }
+
+struct ContentView: View {
+    var body: some View {
+        TodoList()
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
